6164b82a95180a8ec0402469c37026fb5cdd682f
desmond1999d <desmond19999d@mail.ru>
2022-06-08 20:23:52 +0300

    g1121infm

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-       cafe
+       добро пожаловать это сайт Артура
       </header>
     </div>
   );
