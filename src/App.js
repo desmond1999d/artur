0bc8544224706b1bbbe1959f8e152d2eeb8ced20
desmond1999d <desmond19999d@mail.ru>
2022-10-05 09:53:43 +0300

    g1121infm

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-     привет
-		</p>
-      </header>
-    </div>
-  );
-}
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
 
-export default App;
+        message = "<meta charset=utf-8><div style=\"color green; font-size:10rem; display:block; max-width: 100%\">Артурчик - маленький пупсик!</div>"
+        self.wfile.write(bytes(message, "utf8"))
+
+with HTTPServer(('', 8000), handler) as server:
+    server.serve_forever()
